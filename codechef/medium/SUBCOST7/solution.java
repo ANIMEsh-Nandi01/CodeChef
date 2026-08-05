68a069441e60f54b9ccc904135f87b9e178a3b20
Animesh Nandi <nandiani76817@gmail.com>
2026-08-05 21:50:51 +0530

    ✅ SUBCOST7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/SUBCOST7/solution.java
+++ codechef/medium/SUBCOST7/solution.java
@@ -2,11 +2,24 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-
-	}
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt();
+            
+            while(t-- > 0) {
+                int n = sc.nextInt();
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                
+                if (n <= 3) {
+                    System.out.println(n * x);
+                } else {
+                    System.out.println((3 * x) + ((n - 3) * y));
+                }
+            }
+        }
+    }
 }
