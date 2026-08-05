6dea1e3c4c78277a44b890bf85b1eb9e08ba0f5d
Animesh Nandi <nandiani76817@gmail.com>
2026-08-05 23:02:05 +0530

    ✅ PPT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-05

--- codechef/medium/PPT/solution.java
+++ codechef/medium/PPT/solution.java
@@ -1,12 +1,18 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Scanner;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-
-	}
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        if (sc.hasNextInt()) {
+        
+            int n = sc.nextInt();
+            int totalSlidesRequired = 20;
+            int remainingSlides = totalSlidesRequired - n;
+            
+            System.out.println(remainingSlides);
+        }
+        
+        sc.close();
+    }
 }
